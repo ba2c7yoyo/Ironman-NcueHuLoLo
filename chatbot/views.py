c8de22d0415cd1a11d53caf3c210fac98190d334
ba2c7yoyo <a26831302@gmail.com>
2024-09-24 22:43:43 +0800

    feat: day09-TextSendMessage and views.py

--- chatbot/views.py
+++ chatbot/views.py
@@ -6,8 +6,11 @@ from django.conf import settings
 from django.http import HttpResponseForbidden, HttpResponse
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, ImageSendMessage
-import json
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
 
+import json
+from .models import *
+ 
 parser = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
@@ -40,17 +43,24 @@ def callback(request):
 # 處理訊息的事件
 @parser.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 取得貓咪圖片的 URL
-    image_url = reply_cat_url()
+    user_message = event.message.text  # 取得使用者發送的文字
+    if "-" in user_message:
+        teacher_name = user_message.split("-")[0]
+        course_name = user_message.split("-")[1]
+        filtered_courses = Course.objects.filter(teacher_name=teacher_name, course_name=course_name)[:5]
+        if filtered_courses.exists():
+            messages = []
+            for course in filtered_courses:
+                messages.append(TextMessage(
+                    text=f"""課程: {course.course_name}\
+                    評價: {course.feedback_content}
+                    """))
 
-    # 建立要回傳的 ImageSendMessage
-    image_message = ImageSendMessage(
-        original_content_url=image_url,
-        preview_image_url=image_url
-    )
-
-    # 回覆使用者的訊息
-    line_bot_api.reply_message(
-        event.reply_token,
-        image_message
-    )
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages  
+                
+            )         
+        else:
+            print("Empty")
+    
