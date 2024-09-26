52eb8918e02caf197d22506686d0db65475e51a0
ba2c7yoyo <a26831302@gmail.com>
2024-09-26 12:48:36 +0800

    feat: day11-PostbackEvent

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.http import HttpResponseForbidden, HttpResponse
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, FlexSendMessage
+from linebot.models import MessageEvent, PostbackEvent, TextMessage, TextSendMessage, ImageSendMessage, FlexSendMessage
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 
 import json
@@ -18,6 +18,50 @@ parser = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+def teacher_lists_flex_message_package(teacher_name, candidate_courses):
+    #引入 JSON 檔案
+    json_path = os.path.join(BASE_DIR, 
+        'chatbot', 'reply_course_list.json')
+    flex = json.load(open(json_path, 'r', encoding='utf-8'))    
+
+    # 設定標題為老師名稱
+    flex['body']['contents'][0]['text'] = f"{teacher_name}老師的哪堂課?"
+
+    #課程列表的選項用了十個顏色!
+    colors = ['#F0C29E', '#A1DE95', '#F5C578', '#91D9C2', '#DFC493', 
+            '#F0C29E', '#A1DE95', '#F5C578', '#91D9C2', '#DFC493']
+
+    # 根據上述顏色動態生成課程按鈕，而且是根據幾堂課就產生幾個按鈕~
+    for i, course in enumerate(candidate_courses, start=1):
+        button = {
+            'type': 'box',
+            'layout': 'vertical',
+            'spacing': 'none',
+            'contents': [{
+                'type': 'button',
+                'style': 'primary',
+                'action': {
+                    'type': 'postback',
+                    'label': course,
+                    #注意! 這很重要，此處代表按鈕背後真正的資料
+                    'data': f"{teacher_name}-{course}" 
+                },
+                'color': colors[i-1],
+                'margin': 'xs',
+                'offsetTop': 'none',
+                'height': 'sm'
+            }],
+            'flex': 0,
+            'borderWidth': 'medium',
+            'cornerRadius': 'xxl',
+            'offsetTop': 'none',
+            'margin': 'md',
+            'backgroundColor': colors[i-1]
+        }
+        flex['body']['contents'].append(button)
+
+    return flex
 
 def flex_message_package(course_info):
     json_path = os.path.join(BASE_DIR, 'chatbot', 'reply_evaluation.json')
@@ -33,13 +77,6 @@ def flex_message_package(course_info):
     flex['footer']['contents'][0]['contents'][1]['text'] = f"第{course_info['number']}則"
 
     return flex
-
-def reply_cat_url():
-    url = "https://api.thecatapi.com/v1/images/search?limit=1"
-    response = requests.get(url)
-    content = json.loads(response.text)
-    image_url = content[0]["url"]
-    return image_url
 
 
 @csrf_exempt
@@ -60,18 +97,52 @@ def callback(request):
 
     return HttpResponse('OK')
 
-
 # 處理訊息的事件
 @parser.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_teacher_name_msg(event):    
     user_message = event.message.text  # 取得使用者發送的文字
-    if "-" in user_message:
-        teacher_name = user_message.split("-")[0]
-        course_name = user_message.split("-")[1]
-        filtered_courses = Course.objects.filter(teacher_name=teacher_name, course_name=course_name)[:5]
+    filtered_teacher = Course.objects.filter(teacher_name=user_message)
+    if filtered_teacher.exists():
+        teacher_name = user_message
+
+        #values_list 是以陣列裝課程名稱
+        #distinct 可以把可能重複的老師名做過濾
+        candidate_courses = filtered_teacher.values_list(
+        'course_name', flat=True).distinct()
+
+        #稍後會製作這個漂亮的函式，先呼叫它
+        flex_message = teacher_lists_flex_message_package(
+        teacher_name, candidate_courses)
+
+        message = FlexSendMessage(
+                            alt_text=f"{teacher_name} 老師的課程",
+                            contents=flex_message
+                        )
+        line_bot_api.reply_message(
+                    event.reply_token,
+                    message)
+    else:
+        print("Empty")   
+
+@parser.add(PostbackEvent)
+def handle_postback(event):
+    #取得使用者點按鈕時回傳的資料
+    postback_data = event.postback.data
+
+    #還記得前面提及按鈕背後的資料嗎? 在這!
+    #postback_data 格式為 "課程名稱-老師名稱"
+    if "-" in postback_data:
+        teacher_name = postback_data.split("-")[0]
+        course_name = postback_data.split("-")[1]
+
+        filtered_courses = Course.objects.filter(
+            teacher_name=teacher_name, course_name=course_name)[:5]
         if filtered_courses.exists():
             messages = []
-            for idx, course in enumerate(filtered_courses, start=1):  # 使用 enumerate 給每個課程加上編號
+
+            # 使用 enumerate 給每個課程加上編號
+            for idx, course in enumerate(filtered_courses, start=1):  
+
                 # 準備課程資料以傳入 flex_message_package 函式
                 course_info = {
                     'course_name': course.course_name,
@@ -82,17 +153,21 @@ def handle_message(event):
                     'submitter_name': course.submitter_name,
                     'number': str(idx),  # 這裡的 idx 是第幾則的意思
                     }
+
                 # 呼叫 flex_message_package 函式來產生 Flex Message
                 flex_message = flex_message_package(course_info)
+
                 # 使用 FlexSendMessage 回傳 Flex Message
                 messages.append(FlexSendMessage(
                         alt_text=f"課程評價：{course.course_name}",
                         contents=flex_message
                     ))
+
             line_bot_api.reply_message(
                 event.reply_token,
                 messages  # 回傳 Flex Message 列表
             )         
         else:
             print("Empty")
-    
+    else:
+        print("Empty")
